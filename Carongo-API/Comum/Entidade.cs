72187b1c8fba60415761d6eef66f3e38d507c4b0
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-03-02 20:31:24 -0300

    Solução e projetos criados. Entidade base criada

---
+++ Carongo-API/Comum/Entidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Comum
+{
+    public abstract class Entidade
+    {
+        public Guid Id { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+
+        public Entidade()
+        {
+            Id = Guid.NewGuid();
+            DataCriacao = DateTime.Now;
+        }
+    }
+}
