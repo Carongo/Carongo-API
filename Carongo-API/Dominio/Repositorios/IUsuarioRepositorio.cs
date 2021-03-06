a5214951f9089945b0c7ef2e30bcb99ddb3104b8
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-03-06 12:34:41 -0300

    Interface do repositório de Usuario e arquivo de repositorio de Usuario criados

---
+++ Carongo-API/Dominio/Repositorios/IUsuarioRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace Dominio.Repositorios
+{
+    public interface IUsuarioRepositorio
+    {
+        List<Usuario> Listar(string nome = null);
+        Usuario Buscar(Guid id);
+        Usuario Buscar(string email);
+        Usuario Cadastrar(Usuario usuario);
+        Usuario Alterar(Usuario usuario);
+        void Deletar(Guid id);
+    }
+}
---
+++ Carongo-API/Infra/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using Dominio.Entidades;
+using Dominio.Repositorios;
+using System;
+using System.Collections.Generic;
+
+namespace Infra.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public List<Usuario> Listar(string nome = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Buscar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Buscar(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Cadastrar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Alterar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
