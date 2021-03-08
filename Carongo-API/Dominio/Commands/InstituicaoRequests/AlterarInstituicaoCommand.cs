d213f09cd10abbcc47e336b65557acd0bd1ad92c
kaiquethedog <52429335+kaiquethedog@users.noreply.github.com>
2021-03-08 19:51:06 -0300

    Adicionado os commands criar e deletar instituicao

--- Carongo-API/Dominio/Commands/InstituicaoRequests/AlterarInstituicaoCommand.cs
+++ Carongo-API/Dominio/Commands/InstituicaoRequests/AlterarInstituicaoCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Comum.Commands;
+using Flunt.Notifications;
+using Flunt.Validations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,25 @@ using System.Threading.Tasks;
 
 namespace Dominio.Commands.InstituicaoRequests
 {
-    class AlterarInstituicaoCommand
+    public class AlterarInstituicaoCommand : Notifiable<Notification>, ICommand
     {
+        public Guid IdInstituicao { get; set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+
+        public AlterarInstituicaoCommand(string nome, string descricao)
+        {
+           Nome = nome.Trim();
+           Descricao = descricao.Trim();
+        }
+
+
+        public void Validar()
+        {
+            AddNotifications(new Contract<AlterarInstituicaoCommand>()
+                 .Requires()
+                 .IsTrue((Nome.Length > 3) && (Nome.Length < 255), "Nome", "Nome inválido!")
+                 .IsTrue((Descricao.Length > 10) && (Descricao.Length < 512), "Descricao", "Descricao inválida!"));
+        }
     }
 }
